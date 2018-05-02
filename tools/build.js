08b6d68e16f202934cbcba7eaef6eb407f156817
Rajat Sharma <lunasunkaiser@gmail.com>
2018-05-03 02:10:59 +0530

    Fixed file copy to dist

--- tools/build.js
+++ tools/build.js
@@ -1,3 +1,11 @@
 const chainedpack = require('./webpack.config')
+const fs = require('fs-extra')
+const path = require('path')
+const project = require('../project.config')
+
+fs.copySync(
+  path.resolve(project.basePath, 'public'),
+  path.resolve(project.basePath, project.outDir)
+)
 
 chainedpack.build()
