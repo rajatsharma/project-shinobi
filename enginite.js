7a9ef8fd00d39a62243946126beb5d031eda776e
Rajat Sharma <lunasunkaiser@gmail.com>
2019-03-05 22:15:22 +0530

    Added public folder

--- enginite.js
+++ enginite.js
@@ -1,5 +1,5 @@
 module.exports = {
-  template: ['src', '.gitignore', '.editorconfig'],
+  template: ['src', '.gitignore', '.editorconfig', 'public'],
   requireables: ['scripts/dev.js', 'scripts/build.js'],
   packageScripts: ['dev', 'build'],
   bin: 'shinobi',
