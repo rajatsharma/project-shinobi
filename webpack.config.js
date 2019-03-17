82b9f3065907a0520e908b1aa72cb1bc2b60664e
Rajat Sharma <lunasunkaiser@gmail.com>
2019-03-17 17:54:33 +0530

    Changed target node config

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-module.exports = (config, _options, webpack) => {
-  config.devtool = 'none';
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      __WORD__: JSON.stringify('hi'),
-    }),
-  );
-};
