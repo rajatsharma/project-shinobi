8e5e10c837bcc6d1b393fa9cd8cfa899cc27ef1c
Rajat Sharma <lunasunkaiser@gmail.com>
2022-01-12 11:01:47 +0530

    Move to vite

--- __tests__/manifest.js
+++
@@ -1,7 +0,0 @@
-import { inspectManifest } from '@enginite/standard';
-import test from 'ava';
-
-test('Manifest must include mandatory keys', t => {
-  inspectManifest();
-  t.pass();
-});
--- scripts/wrap.js
+++
@@ -1,1 +0,0 @@
-require('@enginite/standard').wrap();
