c7a36d65720a1102cdcdd80c6c8f9abee8f75c6e
Rajat Sharma <lunasunkaiser@gmail.com>
2022-01-12 17:13:51 +0530

    Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Subject } from "rxjs";
 import { map } from "rxjs/operators";
 import useObservable from "use-observable";
+import { text, bg } from "./styles/app.css";
 
 const App$ = new Subject<string>();
 
@@ -14,7 +15,17 @@ function App() {
   const onChange = (e: React.FormEvent<HTMLInputElement>) =>
     App$.next(e.currentTarget.value);
 
-  return <input autoFocus onChange={onChange} value={value} />;
+  return (
+    <div
+      className={bg}
+      style={{
+        background:
+          "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAOklEQVQoU43MSwoAMAgD0eT+h7ZYaOlHo7N+DNHL2HAGgBWcyGcKbqTghTL4oQiG6IUpOqFEC5bI4QD8PAoKd9j4XwAAAABJRU5ErkJggg==",
+      }}
+    >
+      <input className={text} autoFocus onChange={onChange} value={value} />
+    </div>
+  );
 }
 
 export default App;
--- src/styles/main.css.ts
+++ src/styles/main.css.ts
@@ -1,5 +1,29 @@
-import { style } from "@vanilla-extract/css";
+import { globalStyle } from "@vanilla-extract/css";
 
-export const flex = style({
+globalStyle("body", {
   display: "flex",
+  margin: 0,
+  overflowX: "hidden",
+  overflowY: "hidden",
+  background: "#ececec",
+  color: "#151515",
+  border: "solid 9px #4ee58b",
+  minHeight: "98vh",
+});
+
+globalStyle("#root", {
+  display: "flex",
+  width: "100%",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+globalStyle("input", {
+  border: "none",
+  fontSize: 24,
+});
+
+globalStyle("input:focus-visible", {
+  outline: "none",
 });
